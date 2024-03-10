ae55f9b910e7e40a12bfe24950b03db7007c8cc5
yana <yana2002bu@gmail.com>
2024-03-10 00:23:02 -0800

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 /* eslint-disable react/no-unescaped-entities */
-import './App.css'
-import profilePic from './assets/images/avatar-jessica.jpeg'
+import "./App.css";
+import profilePic from "./assets/images/avatar-jessica.jpeg";
 
-function Link(){
-  return <a href="#">link</a>
+function Link({ linkName }) {
+  return (
+    <a href="#" className="main-link">
+      {linkName}
+    </a>
+  );
 }
 
 function App() {
-
   return (
     <main className="main">
       <div className="profile-title">
-        <img src={profilePic} alt="avatar image" />
-        <h3 className='name'>Jessica Randall</h3>
+        <img className="profile-img" src={profilePic} alt="avatar image" />
+        <h3 className="name">Jessica Randall</h3>
         <p className="location">London, United Kingdom</p>
         <p className="description">"Front-end developer and avid reader."</p>
       </div>
       <div className="links">
-        <Link />
-        <Link />
-        <Link />
-        <Link />
-        <Link />
+        <Link linkName={"GitHub"} />
+        <Link linkName={"Frontend Mentor"} />
+        <Link linkName={"LinkedIn"} />
+        <Link linkName={"Twitter"} />
+        <Link linkName={"Instagram"} />
       </div>
     </main>
-  )
+  );
 }
 
-
-export default App
+export default App;
